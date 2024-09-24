9d2bef544a2ec24e32bf4d1b310e4f59cc32ec99
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2024-09-24 09:24:42 +0800

    Light.java

--- Light.java
+++ Light.java
@@ -3,6 +3,8 @@ public class Light implements HomeService {
     public void turnOn() {
         System.out.println("Light is now on");
     }
+
+    @Override
     public void turnOff() {
         System.out.println("Light is now off");
     }
